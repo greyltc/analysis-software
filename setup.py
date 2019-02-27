29f1374fde9135b8f98164d5db5f268c85c6b466
Grey Christoforo <grey@christoforo.net>
2019-02-27 00:34:18 +0000

    make csv dumper

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mutovis-analysis",
-    version="3.0.0",
+    version="3.0.1",
     author="Grey Christoforo",
     author_email="grey@mutovis.com",
     description="Software for analyzing solar cell i-v curves",
@@ -14,6 +14,7 @@ setuptools.setup(
     url="https://github.com/mutovis/analysis-software",
     packages=setuptools.find_packages(),
     entry_points={'gui_scripts': ['mutovis-analysis = batch_iv_analysis:main', ],},
+    data_files=[('bin',['util/h52csv'])],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL-3.0",
